9a275f84b8aa5f0cee50e586e284e8c3ed95ade5
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-09 10:30:21 +0300

    [1] DTW construction - Red

---
+++ DTW/CoreTest/DTW.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTest
+{
+    [TestClass]
+    public class DTW
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            Core.DTW dtw = new Core.DTW();
+        }
+    }
+}
