3bf7ba46e7fc8a26af6466bfe065f478149138d8
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-10 09:19:35 +0300

    [1] IDisposable construction - Red

---
+++ DTW/CoreTest/IDistansable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CoreTest
+{
+    class C : Core.IDistansable
+    {
+        public double DistanceTo(Core.IDistansable rhs)
+        {
+            return 0;
+        }
+    }
+
+    [TestClass]
+    public class IDistansable
+    {
+        [TestMethod]
+        public void Constructor()
+        {
+            new C();
+        }
+    }
+}
