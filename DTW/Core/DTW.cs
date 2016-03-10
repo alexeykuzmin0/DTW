9f262b0f0a127a0944a15d3429b20634e4766a02
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-10 10:11:58 +0300

    [1] DTW ProcessOne - Green

--- DTW/Core/DTW.cs
+++ DTW/Core/DTW.cs
@@ -13,11 +13,22 @@ namespace Core
 
         public DTW(IEnumerable<T> pattern)
         {
-            pattern = new List<T>(pattern);
+            this.pattern = new List<T>(pattern);
             dp = new List<List<double>>();
             dp.Add(new List<double>(pattern.Count() + 1));
             dp[0].Add(0.0);
             dp[0].AddRange(Enumerable.Repeat(double.PositiveInfinity, pattern.Count()));
+        }
+
+        public void Process(T input)
+        {
+            dp.Add(new List<double>(pattern.Count() + 1));
+            dp.Last().Add(0.0);
+            for (int i = 0; i < pattern.Count(); ++i)
+            {
+                dp.Last().Add(Math.Min(dp[dp.Count() - 2][i + 1],
+                    Math.Min(dp[dp.Count() - 2][i], dp.Last()[i]) + pattern[i].DistanceTo(input)));
+            }
         }
 
         public double GetResult()
