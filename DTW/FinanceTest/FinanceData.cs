399f1c73d0bfb9804fb916bf299ce47db4b0f4d1
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 19:39:46 +0300

    [6] Candle equality - RED

--- DTW/FinanceTest/FinanceData.cs
+++ DTW/FinanceTest/FinanceData.cs
@@ -44,5 +44,36 @@ namespace FinanceTest
             Assert.AreEqual(53.61, c.low);
             Assert.AreEqual(53.99, c.close);
         }
+
+        [TestMethod]
+        public void Equality()
+        {
+            var captions = new System.Collections.Generic.Dictionary<string, int>();
+            captions["<DATE>"] = 0;
+            captions["<TIME>"] = 1;
+            captions["<OPEN>"] = 2;
+            captions["<HIGH>"] = 3;
+            captions["<LOW>"] = 4;
+            captions["<CLOSE>"] = 5;
+            string[] values = "20150105;100100;54.0300000;54.4000000;53.6100000;53.9900000;784480".Split(';');
+            var b = new Finance.Candle(captions, values);
+            var e = new Finance.Candle(b);
+            var c0 = new Finance.Candle(b);
+            c0.timestamp.AddDays(1);
+            var c1 = new Finance.Candle(b);
+            c1.open = 1;
+            var c2 = new Finance.Candle(b);
+            c2.high = 1;
+            var c3 = new Finance.Candle(b);
+            c3.low = 1;
+            var c4 = new Finance.Candle(b);
+            c4.close = 1;
+            Assert.AreEqual(true, b == e);
+            Assert.AreEqual(false, b == c0);
+            Assert.AreEqual(false, b == c1);
+            Assert.AreEqual(false, b == c2);
+            Assert.AreEqual(false, b == c3);
+            Assert.AreEqual(false, b == c4);
+        }
     }
 }
