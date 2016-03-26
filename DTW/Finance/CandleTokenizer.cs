bb9326d60a583556220bf8427f896a61b4bdc9fa
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 19:31:16 +0300

    [6] CandleTokenizer constructor - REFACTOR

--- DTW/Finance/CandleTokenizer.cs
+++ DTW/Finance/CandleTokenizer.cs
@@ -8,9 +8,35 @@ namespace Finance
 {
     public class CandleTokenizer
     {
+        List<Candle> candles;
         public CandleTokenizer(System.IO.StreamReader sr)
         {
+            string[] captions = sr.ReadLine().Split(';');
+            Dictionary<string, int> ids = new Dictionary<string, int>();
+            for (int i = 0; i < captions.Count(); ++i)
+            {
+                ids[captions[i]] = i;
+            }
+            CheckCaptions(ids);
+            candles = new List<Candle>();
+            while (!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(';');
+                candles.Add(new Candle(ids, line));
+            }
+        }
 
+        private static void CheckCaptions(Dictionary<string, int> ids)
+        {
+            if (!ids.ContainsKey("<DATE>") ||
+                !ids.ContainsKey("<TIME>") ||
+                !ids.ContainsKey("<OPEN>") ||
+                !ids.ContainsKey("<HIGH>") ||
+                !ids.ContainsKey("<LOW>") ||
+                !ids.ContainsKey("<CLOSE>"))
+            {
+                throw new FormatException("CSV file contains not all required columns");
+            }
         }
     }
 }
