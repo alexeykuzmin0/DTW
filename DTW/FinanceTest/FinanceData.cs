6083742f6e5378c9f42e24e080ed6adb05bf620e
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 19:45:57 +0300

    [6] Candle FieldByFieldConstructor - RED

--- DTW/FinanceTest/FinanceData.cs
+++ DTW/FinanceTest/FinanceData.cs
@@ -26,6 +26,17 @@ namespace FinanceTest
         }
 
         [TestMethod]
+        public void FieldByFieldConstructor()
+        {
+            Finance.Candle c = new Finance.Candle(new DateTime(2015, 1, 5, 10, 1, 0), 54.03, 54.4, 53.61, 53.99);
+            Assert.AreEqual(new DateTime(2015, 1, 5, 10, 1, 0), c.timestamp);
+            Assert.AreEqual(54.03, c.open);
+            Assert.AreEqual(54.4, c.high);
+            Assert.AreEqual(53.61, c.low);
+            Assert.AreEqual(53.99, c.close);
+        }
+
+        [TestMethod]
         public void CopyConstructor()
         {
             var captions = new System.Collections.Generic.Dictionary<string, int>();
