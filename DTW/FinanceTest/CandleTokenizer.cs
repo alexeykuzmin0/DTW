57a0666b84a245200fa6dd3ca94e5789cad53f41
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 20:52:28 +0300

    [6] CandleTokenizer Save - RED

--- DTW/FinanceTest/CandleTokenizer.cs
+++ DTW/FinanceTest/CandleTokenizer.cs
@@ -46,5 +46,22 @@ namespace FinanceTest
             Assert.AreEqual(c1, ct[0]);
             Assert.AreEqual(c2, ct[1]);
         }
+
+        [TestMethod]
+        public void Save()
+        {
+            Finance.CandleTokenizer ct = new Finance.CandleTokenizer(
+                new System.IO.StreamReader(GenerateStream(SAMPLE)));
+            var ms = new System.IO.MemoryStream();
+            ct.Save(new System.IO.StreamWriter(ms));
+            var sr = new System.IO.StreamReader(ms);
+            var line1 = sr.ReadLine();
+            var line2 = sr.ReadLine();
+            var line3 = sr.ReadLine();
+            Assert.AreEqual(true, sr.EndOfStream);
+            Assert.AreEqual("<DATE>;<TIME>;<OPEN>;<HIGH>;<LOW>;<CLOSE>", line1);
+            Assert.AreEqual("20150105;100100;54.03;54.4;53.61;53.99", line2);
+            Assert.AreEqual("20150105;100200;53.99;54.05;53.72;53.72", line3);
+        }
     }
 }
