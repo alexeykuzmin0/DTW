ab894ab765dd318ad83b175cf5b62e1bd0e60a9e
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 19:29:20 +0300

    [6] Candle constructor - GREEN

--- DTW/Finance/FinanceData.cs
+++ DTW/Finance/FinanceData.cs
@@ -10,5 +10,20 @@ namespace Finance
     {
         public DateTime timestamp;
         public double open, high, low, close;
+
+        public Candle(Dictionary<string, int> captions, string[] values)
+        {
+            string date = values[captions["<DATE>"]];
+            string time = values[captions["<TIME>"]];
+            timestamp = DateTime.ParseExact(
+                date + ';' + time,
+                "yyyyMMdd;HHmmss",
+                new System.Globalization.DateTimeFormatInfo());
+            var provider = new System.Globalization.NumberFormatInfo() { NumberDecimalSeparator = "." };
+            open = Convert.ToDouble(values[captions["<OPEN>"]], provider);
+            high = Convert.ToDouble(values[captions["<HIGH>"]], provider);
+            low = Convert.ToDouble(values[captions["<LOW>"]], provider);
+            close = Convert.ToDouble(values[captions["<CLOSE>"]], provider);
+        }
     }
 }
--- DTW/FinanceTest/FinanceData.cs
+++ DTW/FinanceTest/FinanceData.cs
@@ -17,7 +17,7 @@ namespace FinanceTest
             captions["<LOW>"] = 4;
             captions["<CLOSE>"] = 5;
             string[] values = "20150105;100100;54.0300000;54.4000000;53.6100000;53.9900000;784480".Split(';');
-            Core.Candle c = new Finance.Candle(captions, values);
+            Finance.Candle c = new Finance.Candle(captions, values);
             Assert.AreEqual(new DateTime(2015, 1, 5, 10, 1, 0), c.timestamp);
             Assert.AreEqual(54.03, c.open);
             Assert.AreEqual(54.4, c.high);
