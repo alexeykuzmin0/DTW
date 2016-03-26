7ec3d7542216e45c2f2c5de9d2942e5be5725a9f
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-26 18:27:28 +0300

    [6] Candle class added

---
+++ DTW/Finance/FinanceData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Finance
+{
+    public class Candle
+    {
+        public DateTime timestamp;
+        public double open, high, low, close;
+    }
+}
