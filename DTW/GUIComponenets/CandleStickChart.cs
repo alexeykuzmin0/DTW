b8881128fbfd6857b7cade1d41702795dec6465c
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-27 13:21:02 +0300

    [6] CandleStickChart SetCandles - GREEN

--- DTW/GUIComponenets/CandleStickChart.cs
+++ DTW/GUIComponenets/CandleStickChart.cs
@@ -3,16 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Finance;
 
 namespace GUIComponents
 {
     public class CandleStickChart : ZedGraph.ZedGraphControl
     {
+        Finance.AbstractCandleTokenizer candles;
+
         public CandleStickChart()
         {
             GraphPane.Title.Text = "No data loaded";
             GraphPane.XAxis.Title.Text = "";
             GraphPane.YAxis.Title.Text = "";
+        }
+
+        public void SetCandles(CandleTokenizer tokenizer)
+        {
+            candles = tokenizer;
+            var points = new ZedGraph.StockPointList();
+            for (int i = 0; i < candles.GetLength(); ++i)
+            {
+                points.Add(new ZedGraph.StockPt(
+                    i,
+                    candles[i].high,
+                    candles[i].low,
+                    candles[i].open,
+                    candles[i].close,
+                    0));
+            }
+            var curve = GraphPane.AddJapaneseCandleStick("", points);
         }
     }
 }
