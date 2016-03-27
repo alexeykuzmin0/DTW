77fe606000df27f276ee1711280fb5e86603229d
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-27 13:14:08 +0300

    [6] CandleStickChart setCandles - RED

--- DTW/GUIComponentsTest/CandleStickChart.cs
+++ DTW/GUIComponentsTest/CandleStickChart.cs
@@ -6,6 +6,21 @@ namespace GUIComponentsTest
     [TestClass]
     public class CandleStickChart
     {
+        public static System.IO.Stream GenerateStream(string s)
+        {
+            System.IO.MemoryStream stream = new System.IO.MemoryStream();
+            System.IO.StreamWriter writer = new System.IO.StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        const string SAMPLE =
+            "<DATE>;<TIME>;<OPEN>;<HIGH>;<LOW>;<CLOSE>;<VOL>\n" +
+            "20150105;100100;54.0300000;54.4000000;53.6100000;53.9900000;784480\n" +
+            "20150105;100200;53.9900000;54.0500000;53.7200000;53.7200000;287400";
+
         [TestMethod]
         public void Constructor()
         {
@@ -13,6 +28,17 @@ namespace GUIComponentsTest
             Assert.AreEqual("No data loaded", csc.GraphPane.Title.Text);
             Assert.AreEqual("", csc.GraphPane.XAxis.Title.Text);
             Assert.AreEqual("", csc.GraphPane.YAxis.Title.Text);
+        }
+
+        [TestMethod]
+        public void SetCandles()
+        {
+            var tokenizer = new Finance.CandleTokenizer(
+                new System.IO.StreamReader(GenerateStream(SAMPLE)));
+            var csc = new GUIComponents.CandleStickChart();
+            csc.SetCandles(tokenizer);
+            Assert.AreEqual(1, csc.GraphPane.CurveList.Count);
+            Assert.AreEqual(2, csc.GraphPane.CurveList[0].NPts);
         }
     }
 }
