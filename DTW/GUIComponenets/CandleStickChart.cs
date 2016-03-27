f141297a0c7855fd7fa3bf284339ef5836ff62b0
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-03-27 14:21:51 +0300

    [6] CandleStickChart ticker and period processing - GREEN

--- DTW/GUIComponenets/CandleStickChart.cs
+++ DTW/GUIComponenets/CandleStickChart.cs
@@ -18,14 +18,30 @@ namespace GUIComponents
             GraphPane.YAxis.Title.Text = "";
         }
 
+        private string CreatePeriodString(TimeSpan period)
+        {
+            if (period.Days > 0)
+            {
+                return period.Days.ToString() + " day" + (period.Days > 1 ? "s" : "");
+            }
+            if (period.Hours > 0)
+            {
+                return period.Hours.ToString() + " hour" + (period.Hours > 1 ? "s" : "");
+            }
+            return period.Minutes.ToString() + " minute" + (period.Minutes > 1 ? "s" : "");
+        }
+
         public void SetCandles(AbstractCandleTokenizer tokenizer)
         {
             GraphPane.CurveList.Clear();
             candles = tokenizer;
             if (tokenizer == null)
             {
+                GraphPane.Title.Text = "No data loaded";
                 return;
             }
+            GraphPane.Title.Text = tokenizer.GetTicker() + " " + 
+                CreatePeriodString(tokenizer.GetPeriod());
             var points = new ZedGraph.StockPointList();
             for (int i = 0; i < candles.GetLength(); ++i)
             {
