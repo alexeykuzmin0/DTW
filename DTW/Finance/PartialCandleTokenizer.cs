ece8e459c611b3a140fd36b76568402cc4f64436
Кузьмин Алексей Андреевич <alexeykuzmin0@gmail.com>
2016-04-03 20:55:41 +0300

    [6] PartialCandleTokenizer - GREEN

--- DTW/Finance/PartialCandleTokenizer.cs
+++ DTW/Finance/PartialCandleTokenizer.cs
@@ -8,17 +8,28 @@ namespace Finance
 {
     public class PartialCandleTokenizer : AbstractCandleTokenizer
     {
+        private AbstractCandleTokenizer ct;
+        private int start;
+        private int length;
+
+        public PartialCandleTokenizer(AbstractCandleTokenizer ct, int start, int length)
+        {
+            this.ct = ct;
+            this.start = start;
+            this.length = length;
+        }
+
         public override Candle this[int index]
         {
             get
             {
-                throw new NotImplementedException();
+                return ct[index + start];
             }
         }
 
         public override int GetLength()
         {
-            throw new NotImplementedException();
+            return length;
         }
     }
 }
